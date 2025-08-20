8971680bcae3b6997ab4bd792024be92fea0b640
saurav dahal <d.saurab1@gmail.com>
2025-08-20 15:54:23 -0400

    modified drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './netlify/db/user_schema.ts', // <- your schema file
+  schema: './netlify/db/user_schema.ts',
   out: './drizzle',
   dbCredentials: {
     url: process.env.NETLIFY_DATABASE_URL ?? process.env.DATABASE_URL!,
